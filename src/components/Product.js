af128d8544d43a8d4c618cff1e281d63a8f22b3e
IU520CF <lee.ji.eun.ily@outlook.com>
2022-03-08 17:26:22 +0800

    库存UI逻辑处理

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,27 +1,30 @@
 import React from "react";
 // 导入货币组件
-import { formatprice } from "commons/helper"
+import { formatprice } from "commons/helper";
 class Product extends React.Component {
-
- 
   render() {
     // props 传值 数据格式化
-    const {name, tags, image, price } = this.props.product
+    const { name, tags, image, price, status } = this.props.product;
+    const _pClass = {
+      available: 'product',
+      unavailable:'product out-stock'
+    }
     return (
-      <div className="product">
+      <div className={_pClass[status]}>
         <div className="p-content">
           <div className="img-wrapper">
+            <div className="out-stock-text">Out Of Stock</div>
             <figure className="image is-4by3">
-              <img src={image} alt={name}/>
+              <img src={image} alt={name} />
             </figure>
-            <p className="p-tags">{tags}</p>
-            <p className="p-name">{name}</p>
           </div>
+          <p className="p-tags">{tags}</p>
+          <p className="p-name">{name}</p>
         </div>
         <div className="p-footer">
           {/* 使用组件不用this */}
           <p className="price">{formatprice(price)}</p>
-          <button className="add-cart">
+          <button className="add-cart" disabled={status === 'unavailable'}>
             <i className="fas fa-shopping-cart"></i>
             <i className="fas fa-exclamation"></i>
           </button>
